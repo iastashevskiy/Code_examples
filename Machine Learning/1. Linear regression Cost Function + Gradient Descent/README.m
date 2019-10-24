c8bd6675dc91c336f1094dd805893ccd2e98c357
Ilya <45002698+iastashevskiy@users.noreply.github.com>
2019-10-24 15:52:48 +0300

    Update README.m

--- Machine Learning/1. Linear regression Cost Function + Gradient Descent/README.m
+++ Machine Learning/1. Linear regression Cost Function + Gradient Descent/README.m
@@ -1,2 +1,2 @@
-Run ex1.m to start a project. Project implements linear regression with one variable, training set is obtained from ex1data1.txt file. 
-Cost function is computed in computeCost.m,gradient descent is implemented in gradientDescent.m
+% Run ex1.m to start a project. Project implements linear regression with one variable, training set is obtained from ex1data1.txt file. 
+% Cost function is computed in computeCost.m,gradient descent is implemented in gradientDescent.m
